27282a1a3ad6bf6cdd17353ff8060461c8d26921
Alex <ilson.alex@gmail.com>
2024-01-04 19:58:45 -0800

    Added seeds

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,9 +1,27 @@
 const router = require('express').Router();
-// const { User } = require('../../models');
+const { User } = require('../../models');
 
 router.get('/', async (req, res) => {
     res.status(200).json({ message: "It works!" });
     return;
+})
+
+router.get('/usersAll', async (req, res) => {
+
+    try {
+
+        const userData = await User.findAll();
+
+        if (!userData) {
+            res.status(400).json({ message: 'Something went wrong with the database.' });
+            return;
+        }
+
+        res.status(200).json(userData);
+        
+    } catch (err) {
+        res.status(400).json(err);
+    }
 })
 
 router.post('/', async (req, res) => {
